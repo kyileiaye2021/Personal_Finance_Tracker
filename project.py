d3456b46c541998d6477d0ff9fec30043a138b4c
aye, kyi lei <98507949+kyileiaye2021@users.noreply.github.com>
2024-07-02 23:22:04 -0700

    Added add_transaction and load_transaction functions to save and show transaction

--- project.py
+++ project.py
@@ -1,11 +1,42 @@
 #main functions and other functions will be implemented
+import csv
 
 def main():
-    pass
-def func1():
-    pass
-def func2():
-    pass
+    print("Welcome To Personal Finance Tracker")
+    
+def add_transaction(date, category, amount, transaction_type):
+    '''
+    This function will add a transaction (expenses or income) to the csv file
+    '''
+    # Parameters
+    # date(str): The date of transaction (YYYY-MM-DD)
+    # category(str): category of transaction
+    # amount(float): amount of transaction
+    # transaction_type(str): the type of transaction (expenses / income)
+    #open the csv file and append the transaction to the csv file
+    
+    with open('data/transaction.csv', 'a',newline='') as file: #open the file with append mode | no newline will be added
+        writer = csv.writer(file) #create an object to write data to the csv file
+        writer.writerow([date, category, amount, transaction_type])
+        
+def load_transaction():
+    '''
+    This function will return the list of transaction from the csv file.
+    Return type: list
+    '''
+    #open the csv file and read the data
+    transaction_lst = [] #create a list to store transaction lst from csv file
+    try:
+        
+        with open('data/transaction.cv', 'r') as file:
+            reader = csv.reader(file) #create an obj to read data from the file
+            transaction_lst = list(reader) #assign the list of transaction from the csv file
+            
+    except FileNotFoundError:
+        print("Transaction File Not Found")
+        
+    return transaction_lst
+
 def func3():
     pass
 
