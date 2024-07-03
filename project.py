b5e0e246035324886378c96ad629be9a3bfae121
aye, kyi lei <98507949+kyileiaye2021@users.noreply.github.com>
2024-07-03 00:26:00 -0700

    Added test_add_transaction and debug two transaction funcs

--- project.py
+++ project.py
@@ -3,6 +3,12 @@ import csv
 
 def main():
     print("Welcome To Personal Finance Tracker")
+    '''
+    add_transaction('2023-06-10', 'Food', 40, 'Expense')
+    transactions = load_transaction()
+    last_transaction = transactions[-1]
+    print(last_transaction)
+    '''
     
 def add_transaction(date, category, amount, transaction_type):
     '''
@@ -15,7 +21,7 @@ def add_transaction(date, category, amount, transaction_type):
     # transaction_type(str): the type of transaction (expenses / income)
     #open the csv file and append the transaction to the csv file
     
-    with open('data/transaction.csv', 'a',newline='') as file: #open the file with append mode | no newline will be added
+    with open('Data/transactions.csv', 'a',newline='') as file: #open the file with append mode | no newline will be added
         writer = csv.writer(file) #create an object to write data to the csv file
         writer.writerow([date, category, amount, transaction_type])
         
@@ -28,8 +34,9 @@ def load_transaction():
     transaction_lst = [] #create a list to store transaction lst from csv file
     try:
         
-        with open('data/transaction.cv', 'r') as file:
+        with open('Data/transactions.csv', 'r') as file:
             reader = csv.reader(file) #create an obj to read data from the file
+            next(reader) #skip the header row 
             transaction_lst = list(reader) #assign the list of transaction from the csv file
             
     except FileNotFoundError:
--- test_project.py
+++ test_project.py
@@ -1,2 +1,12 @@
 # functions iin the project.py will be tested here
 import pytest
+from project import add_transaction, load_transaction
+
+def test_add_transaction():
+    add_transaction('2024-06-10', 'Food', 40, 'Expense')
+    
+    transactions = load_transaction()
+    last_transaction = transactions[-1]
+    
+    assert last_transaction == ['2024-06-10', 'Food', '40', 'Expense']
+    
