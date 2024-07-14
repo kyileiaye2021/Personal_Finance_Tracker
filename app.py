8b44affe0e857e7ac4abf55a6a8a745677bce643
aye, kyi lei <98507949+kyileiaye2021@users.noreply.github.com>
2024-07-13 19:19:43 -0700

    Added Registration form, login form, registration route, and login route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user, login_required
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm #for creating forms in Flask-WTF
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -30,13 +30,72 @@ class User(db.Model, UserMixin): #User class inherits from db.Model and UserMixi
     email = db.Column(db.String(120), unique=True, nullable=False) #A string column with a maximum length of 120 chars, which must be unique and cannot have null val
     password = db.Column(db.String(60), nullable=False) 
 
-#load user func query the data from database
+#load user func to query the data from database by primary key
+#load user function - call back function to reload user object from the user ID stored in the session
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id)) #get the user object from the database using the user
 
-#load user function - call back function to reload user object from the user ID stored in the session
+#Creating a registration form for user where they can fill input data info and validate the user info
+class RegistrationForm(FlaskForm): #inherited from FlaskForm class which is for creating forms in Flask-WTF
+    username = StringField('Username', validators=[DataRequired(),Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = StringField('Password', validators=[DataRequired()])
+    confirm_password = StringField('Confirm Password', validators=[DataRequired(), EqualTo(password)])
+    submit = SubmitField('Sign Up')
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data).first() # return the first value if the username is found in username column of the User model
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email = email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
+#create a login form for users to login the account
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = StringField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+ 
+#creating route for register page   
+@app.route('/register', methods=['GET', 'POST']) #GET displays the registration form | POST is used to process the form data when the user submits it.
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index')) # if the user is currently logged in (authenticated), redirect back to home page
+    
+    form = RegistrationForm() #create an instance of RegistrationForm class to create a user database
+    if form.validate_on_submit(): #if the form is valid
+        #create a new user obj
+        hashed_password = generate_password_hash(form.password.data) #werkzeug's security module hashes the user's password | Hashing is a one-way process that converts the plain text password into a secure hash, which is then stored in the database.
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password) #create a database row for an individual with the input from the user
+        #add the new user to database
+        db.session.add(user) #add the user to the database session (This session is temporary)
+        db.session.commit() #saving the new user to the database
+        flash('Your account has been created!', 'success') #printing out message
+        return redirect(url_for('login')) #After registering, the login page will be redirected
+    return render_template('register.html', form=form)
+
+# creating route for login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(emai=form.email.data).first() #check the user is in the database. return the first username in the database
+        if user and check_password_hash(user.password, form.password.data): #check if the email exists and the password in the database is the same as provided password in login
+            login_user(user) #logs the user in by creating a session for them. It sets the user ID in the session and tracks the user's authentication status
+            return redirect(url_for('index')) #redirect to homepage after loggin in
+        else:
+            flash('Login Unsuccessful. Please check email and password.', 'danger')
+        return render_template('login.html', form=form)
+    
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html')
 
